fca442e9de106baccd12963668d05af781d895a9
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2020-04-27 15:58:27 -0300

    updating python file

--- analyze_binacle_dataset/query_process.py
+++ analyze_binacle_dataset/query_process.py
@@ -1,6 +1,7 @@
 #Before execute the script, use pip to install google lib
 #Import googlesearch for URL requests 
 from googlesearch import search
+import sys
 
 #File to store query_log
 #query_log = open("AUTO_REPAIRS.txt", "w")
@@ -18,26 +19,36 @@ def listToString(s):
     str1 = " "
     return (str1.join(s))
 
-#Read log file
-n_hash = ("logs/fail/484144517.log")
-log_file = open("logs/fail/484144517.log","r")
-query = lastNlines(log_file,1)
-#for p in query:
-#    print(p)
 
-#Googling error log and show de results, convert lines from list to string before use the query
-query_s = listToString(query)
-query_s = query_s.replace("\n"," ")
-for g in search(query_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
-    url = g
+if __name__ == "__main__":
 
-#Testing query seach on google and show the results
-if ('url' not in locals()):
-    print("No results found using searching terms: ", query_s)
-else:
-    print("Hash: ", n_hash[10:-4])
-    print("Query: ", query_s)
-    print("URL: ", url)
+    #filename="logs/fail/484144517.log"
+    if (len(sys.argv)<2):
+        print("Please, provide a file on input.\nExample format: python query_process.py logs/fail/484144517.log")
+        sys.exit(0)
+    filename=sys.argv[1]        
+    print("Processing file {}".format(filename))
+
+    #Read log file
+    n_hash = (filename)
+    log_file = open(filename,"r")
+    query = lastNlines(log_file,1)
+    #for p in query:
+    #    print(p)
+    
+    #Googling error log and show de results, convert lines from list to string before use the query
+    query_s = listToString(query)
+    query_s = query_s.replace("\n"," ")
+    for g in search(query_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
+        url = g
+
+    #Testing query seach on google and show the results
+    if ('url' not in locals()):
+        print("No results found using searching terms: ", query_s)
+    else:
+        print("Hash: ", n_hash[10:-4])
+        print("Query: ", query_s)
+        print("URL: ", url)
 
 #Write query_log
 #query_log.write("Hash: ", n_hash[10:-4])
