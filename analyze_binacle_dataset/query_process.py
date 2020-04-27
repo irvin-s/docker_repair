06853c745e06628be4b0cdb816e4a08198521639
Irvin <isb@cin.ufpe.br>
2020-04-27 10:57:24 -0300

    adding new features

--- analyze_binacle_dataset/query_process.py
+++ analyze_binacle_dataset/query_process.py
@@ -1,22 +1,30 @@
-#Import urlib for URL requests
-import urllib.request
+#Before execute the script, use pip to install google lib
+#Import googlesearch for URL requests 
+from googlesearch import search
 
 #Function to read log file last lines
+lines = []
 def lastNlines(f,n):
     with f as file:
         for line in (file.readlines()[-n:]):
-            lines = line
+            lines.append(line)
     return lines
+
+#Function to convert list to string
+def listToString(s):
+    str1 = " "
+    return (str1.join(map(str, s)))
 
 #Read log file
 log_file = open("logs/fail/484144517.log","r")
-result = lastNlines(log_file,3)
-print(result)
+query = lastNlines(log_file,5)
+#for p in query:
+#    print(p)
 
-#Send the error log to google
-webUrl = urllib.request.urlopen('http://www.google.com.')
-#print("result code: " + str(webUrl.getcode()))
-
-#Show the google query result
-data = webUrl.read()
-#print(data)
+#Googling error log and show de results, convert lines from list to string before use the query
+query_s = listToString(query)
+try:
+   for g in search(query_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
+        print(g)
+except:
+        print("Error")
