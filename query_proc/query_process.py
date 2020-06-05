5edcce40df9a091f56754d7f36a4b1a054e24d97
Irvin <isb@cin.ufpe.br>
2020-06-05 10:38:02 -0300

    Fully functional

--- query_proc/query_process.py
+++ query_proc/query_process.py
@@ -8,11 +8,7 @@ from googlesearch import search
 import sys
 import json
 import re
-
-#Initializing variables
-url = []
-dataJ = {}
-
+import time
 
 #File to read url white list
 url_white_lst = open("query_url_white_list.txt","r")
@@ -21,55 +17,57 @@ url_white_lst = url_white_lst.read().splitlines()
 #File to store query_log
 query_log = open("../results/analyzed_query.json", "a")
 
-#Function to conver list to dict
-def listToDict(lst):
-    lst = { i : lst[i] for i in range(0, len(lst) ) }
-    return lst
-
-#Function to white listing URLs
-# returns True if was in whiteList else, False
-def checkURL(wUrl):
-    for white_lst in url_white_lst:
-        if re.search(white_lst, wUrl):
-            return True
-    return False
-
 #Function to convert list to string
 def listToString(s):
     str1 = " "
     return (str1.join(s))
 
+#Function to conver list to dict
+def listToDict(lst):
+    lst = { i : lst[i] for i in range(0, len(lst) ) }
+    return lst
+
+#Read the keywords
 def getAllKeyWords():
-    file = '../results/keywords.txt'
+    file = open ( '../results/keywords.txt' , 'r')
     lines = []
-    with open(file, 'r') as reader:
-        for line in reader:
-            line = line.strip().split(', ')
-            lines.append(line)
+    lines = [ line.split() for line in file]
     return lines
 
-# n_hash is the name of file in hash.
-def process(n_hash, keywords):
+#Function to white listing URLs
+def checkURL(wUrl):
+    for white_lst in url_white_lst:
+        if re.search(white_lst, wUrl):
+            result = True
+            break
+        else:
+            result = False
+    return result
+
+# Process the keyword
+def process(n_hash, keyword):
     i = 0
+    url = []
+    dataJ = {}
+
     #Testing query seach on google
-    keyword_s = ''
     while not url:
-        keyword_s = listToString(keywords[:6 + i])
+        keyword_s = listToString(keyword[:6 + i])
+        keyword_s = keyword_s.replace(',','')
         for g in search(keyword_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
             if checkURL(g):
                 url.append(g)
         i += 2
     #for g in search(query_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
     #    url.append(g)
-
+    
     #If the url doesn't match the searching criteria
     if not url:
         url.append("")
-    print("Process finished, for log check ../results/analyzed_query.json")
-
+    
     #Write query_log
-    dataJ['Hash: '+n_hash[13:-4]] = []
-    dataJ['Hash: '+n_hash[13:-4]].append({
+    dataJ['Hash: '+n_hash[13:-5]] = []
+    dataJ['Hash: '+n_hash[13:-5]].append({
         #FIXME @Irvin #'Log fragment': query_s, 
         'Query': keyword_s,
         'URLs': ( listToDict(url) )
@@ -77,9 +75,12 @@ def process(n_hash, keywords):
     json.dump(dataJ, query_log, indent=4)
     query_log.write("\n")
     query_log.write("\n")
-
+    query_log.close
 
 if __name__ == "__main__":
-    lines = getAllKeyWords()
-    for line in lines:
+    keywords = getAllKeyWords()
+    print('Processing...')
+    for line in keywords:
         process(line[0], line[1:])
+        time.sleep(6.0)
+    print("Process finished, for log check ../results/analyzed_query.json")
