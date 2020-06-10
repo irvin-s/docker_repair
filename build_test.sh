25938afbd32cc0c96623285070a2613439c99f07
Irvin <isb@cin.ufpe.br>
2020-06-10 20:30:38 -0300

    filtering broken files

--- build_test.sh
+++ build_test.sh
@@ -11,8 +11,16 @@ mkdir -p ${HERE}/logs/fail
 # File with dockerfile names
 images_file="images_list.txt"
 
+# Verify the sources direcotry
+if [ ! -d ${HERE}/binacle_data/sources ];
+then
+    echo "Please, download the dataset first!"
+    exit
+fi
+
 # Read the file with dockerfile names and try to execute
 while read line; do
+
 	 # parse hashcode
     hash=$(echo $line | cut -f2 -d" " | cut -f2 -d"/" | cut -f1 -d\.)
     logfile=${HERE}/logs/history/$hash.log
@@ -30,11 +38,17 @@ while read line; do
      cp -r ${BINACLE_DIR}/sources/$hash.Dockerfile Dockerfile
      touch $logfile
      echo "Building image binacle:$hash"
+
      ## build image, dump output and error streams to $logfile
      msg_log=$(docker build -t binacle:$hash . 2>&1 )
+
 	  ## removes all images and test the built was successfully or failed
 	  if [[ $msg_log == *"Successfully tagged"* ]];
      then
+        if [[$msg_log ==*"returned a non-zero code"*]];
+        then
+            echo "../logs/fail/$hash.log" > ${HERE}/results/broken_files.log
+        fi
        	echo "$msg_log" > ${HERE}/logs/success/$hash.log 
        	docker system prune -af
      else
