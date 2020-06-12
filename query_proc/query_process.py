2b4f303f34ffe82a8376adec503b85db1e3ee244
Irvin <isb@cin.ufpe.br>
2020-06-12 11:24:47 -0300

    code refectoring

--- query_proc/query_process.py
+++ query_proc/query_process.py
@@ -12,10 +12,7 @@ sys.path.append('../keyword_gen')
 import keyword_creator
 
 #Initializing variables
-url = []
-lines = []
-dataJ = {}
-i = 0
+
 
 #File to read url white list
 url_white_lst = open("query_url_white_list.txt","r")
@@ -24,8 +21,13 @@ url_white_lst = url_white_lst.read().splitlines()
 #File to store query_log
 query_log = open("../results/analyzed_query.json", "a")
 
+#File to read log files
+log_files = open("../results/broken_files.log","r")
+log_files = log_files.read().splitlines()
+
 #Function to read log file last lines
 def lastNlines(f,n):
+    lines = []
     with f as file:
         for line in (file.readlines()[-n:]):
             lines.append(line)
@@ -51,52 +53,67 @@ def checkURL(wUrl):
             result = False
     return result
 
-if __name__ == "__main__":
+#Function to write a query_logjson
+def writetolog(hash, fragment, keyword, url):
+    dataJ = {} 
+    dataJ['Hash: '+n_hash[13:-4]] = []
+    dataJ['Hash: '+n_hash[13:-4]].append({
+    'Log fragment': fragment,
+    'Query': keyword,
+    'URLs': ( listToDict(url) )
+    }) 
+    json.dump(dataJ, query_log, indent=4)
+    query_log.write("\n")
+    query_log.write("\n")
+    query_log.close
 
-    #filename="logs/fail/484144517.log"
-    if (len(sys.argv)<2):
-        print("Please, provide a file on input.\nExample format: python query_process.py ../logs/fail/484144517.log")
-        sys.exit(0)
-    filename=sys.argv[1]        
-    print("Processing file {}".format(filename))
-
-    #Read log file
-    n_hash = (filename)
-    log_file = open(filename,"r")
-    query = lastNlines(log_file,3)
-    #for p in query:
-    #    print(p)
-    
-    #Googling error log and show de results, convert lines from list to string before use the query
-    query_s = listToString(query)
-    query_s = query_s.replace("\n"," ")
-
-    #Gerenating keyword
-    keyword = keyword_creator.keyGen(query_s)
-    
-    #Testing query seach on google
+#Function to query search on google
+def geturl(keyword):
+    url = []
+    i = 0
     while not url:
         keyword_s = listToString(keyword[:6 + i])
         for g in search(keyword_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
             if checkURL(g):
                 url.append(g)
         i += 2
-    #for g in search(query_s, tld="com", lang="en", num=5, start=0, stop=6, pause=2):
-    #    url.append(g)
 
-    #If the url doesn't match the searching criteria
+    #If query doesn't match the searching criteria
     if not url:
         url.append("")
-    print("Process finished, for log check results/analyzed_query.json")
 
-#Write query_log
-dataJ['Hash: '+n_hash[13:-4]] = []
-dataJ['Hash: '+n_hash[13:-4]].append({
-    'Log fragment': query_s,
-    'Query': keyword_s,
-    'URLs': ( listToDict(url) )
-}) 
-json.dump(dataJ, query_log, indent=4)
-query_log.write("\n")
-query_log.write("\n")
-query_log.close
+    res_url = keyword_s, url
+    return res_url
+
+if __name__ == "__main__":
+
+    #filename="logs/fail/484144517.log"
+    for logprocess in log_files:
+      filename = logprocess  
+      #if (len(sys.argv)<2):
+      #  print("Please, provide a file on input.\nExample format: python query_process.py ../logs/fail/484144517.log")
+      #  sys.exit(0)
+      #filename=sys.argv[1]        
+      print("Processing file {}".format(filename))
+
+      #Read log file
+      n_hash = (filename)
+      log_file = open(filename,"r")
+      query = lastNlines(log_file,3)
+      #for p in query:
+      #    print(p)
+    
+      #Convert lines from list to string before use the query
+      query_s = listToString(query)
+      query_s = query_s.replace("\n"," ")
+
+      #Gerenating keyword
+      keyword = keyword_creator.keyGen(query_s)
+    
+      #Testing query search on google
+      url = geturl(keyword)
+      
+      #Write results to log
+      writetolog(n_hash, query_s, url[0], url[1])
+
+print ("Process finished, for log check results/analyzed_query.json")
